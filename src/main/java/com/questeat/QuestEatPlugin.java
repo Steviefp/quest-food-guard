c282795648515f6a8a59d63e1744a1d8125e40e4
steven <sprice7287@gmail.com>
2025-07-20 01:24:04 -0400

    init

---
+++ src/main/java/com/questeat/QuestEatPlugin.java
@@ -0,0 +1,89 @@
+package com.questeat;
+
+import com.google.inject.Provides;
+
+import javax.inject.Inject;
+
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.*;
+import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.gameval.ItemID;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+@PluginDescriptor(
+        name = "Example"
+)
+public class QuestEatPlugin extends Plugin {
+    @Inject
+    private Client client;
+
+    @Inject
+    private QuestEatConfig config;
+
+    private boolean confirmTomatoEat = false;
+
+    // quest edible ID's
+    // EVIL DAVE STEW NOT ADDED
+    private static final Set<Integer> CONFIRM_ITEMS = Set.of(
+            ItemID.TOMATO,
+            ItemID.BANANA,
+            ItemID.CHOCOLATE_BAR,
+            ItemID.CHOCOLATY_MILK,
+            ItemID.CHEESE,
+            ItemID.ORANGE_SLICES,
+            ItemID.HUNDRED_PIRATE_FISHCAKE,
+            ItemID._100_JUBBLY_MEAT_COOKED,
+            ItemID.HUNDRED_ILM_COOKED_STUFFED_SNAKE,
+            ItemID.VILLAGE_RARE_TUBER
+
+    );
+
+
+    /*
+    Look for inventory event
+    check if its edible
+    */
+    @Subscribe
+    public void onMenuEntryAdded(MenuEntryAdded event) {
+        // would like to only check inv
+//        if (event.getActionParam0() != WidgetID.INVENTORY_GROUP_ID)
+//            return;
+
+        MenuEntry[] entries = client.getMenuEntries();
+        List<MenuEntry> newEntries = new ArrayList<>();
+
+        for (MenuEntry entry : entries) {
+            if (!(entry.getOption().equals("Eat") && CONFIRM_ITEMS.contains(entry.getItemId()))) {
+                newEntries.add(entry);
+            }
+        }
+
+// Update menu entries with the filtered list
+        client.setMenuEntries(newEntries.toArray(new MenuEntry[0]));
+    }
+
+
+    @Override
+    protected void startUp() throws Exception {
+        log.info("Example started!");
+    }
+
+    @Override
+    protected void shutDown() throws Exception {
+        log.info("Example stopped!");
+    }
+
+
+    @Provides
+    QuestEatConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(QuestEatConfig.class);
+    }
+}
--- src/test/java/com/questeat/QuestEatTest.java
+++ src/test/java/com/questeat/QuestEatTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.questeat;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class QuestEatTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(QuestEatPlugin.class);
 		RuneLite.main(args);
 	}
 }
